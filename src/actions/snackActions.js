c2e48f95c57b8fb6f3a41a4b146b6b44c672c5ef
Jenna Ransom <jenna.ransom@gmail.com>
2020-06-14 22:38:38 -0600

    add actions folder and snacks fetch action

---
+++ src/actions/snackActions.js
@@ -0,0 +1,14 @@
+export function fetchSnacks(){
+  return async (dispatch) => {
+    try {
+      let res = await fetch('http://localhost:3001/api/v1/snacks')
+      if (!res.ok) {
+        throw res
+      }
+      let snacks = await res.json()
+      dispatch({type: 'ADD_SNACKS', snacks: snacks })
+    } catch(err) {
+      alert(err)
+    }
+  }
+}
