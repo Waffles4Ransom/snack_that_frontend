4dfca273f391276674715650ff438659f798275d
Jenna Ransom <jenna.ransom@gmail.com>
2020-06-14 22:40:37 -0600

    render HomeContainer in App and add action to reducer to add snacks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomeContainer from './containers/HomeContainer'
 
 function App() {
   return (
     <div className="App">
       <h2>Snack That</h2>
+      <HomeContainer />
     </div>
   );
 }
--- src/reducers/snackReducer.js
+++ src/reducers/snackReducer.js
@@ -1,5 +1,8 @@
 export default function snackReducer(state = [], action) {
   switch(action.type) {
+    case 'ADD_SNACKS':
+      return [...state, action.snacks]
+      
     default:
       return state
   }
