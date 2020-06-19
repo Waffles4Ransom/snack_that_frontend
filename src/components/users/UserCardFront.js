b1a87bf0eadac02e67b7af23a0bfbf443a3d097c
Jenna Ransom <jenna.ransom@gmail.com>
2020-06-18 20:40:49 -0600

    give user card image round edges

--- src/components/users/UserCardFront.js
+++ src/components/users/UserCardFront.js
@@ -4,7 +4,7 @@ const UserCardFront = props => {
   return (
     <div className='snackcard' onClick={() => props.handleClick()}>
       <h4>{props.user.name}</h4>
-      <img src={props.user.profile_photo} alt={props.user.name} className='snackImg'/>
+      <img src={props.user.profile_photo} alt={props.user.name} className='userCardImg'/>
       <h5>Username: {props.user.username}</h5>
     </div>
   )
