5f8d9bb2659db2a359fca4cdbd48df5bf238b17f
Jenna Ransom <jenna.ransom@gmail.com>
2020-06-25 17:05:32 -0600

    add join date function to helpers for use in profile and user card back

--- src/components/users/UserCardBack.js
+++ src/components/users/UserCardBack.js
@@ -1,15 +1,11 @@
 import React from 'react'
+import { renderDate } from '../../helpers/helpers'
 
 const UserCardBack = props => {
 
-  const renderDate = () => {
-    let joinDate = new Date(props.user.created_at), options = {year: 'numeric', month: 'long' }
-    return joinDate.toLocaleString('en-US', options)
-  }
-
   return (
     <div className='snackcard' onClick={() => props.handleClick()}>
-      <h4>Joined: {renderDate()}</h4>
+      <h4>Joined: {renderDate(props.user)}</h4>
       <h5>Hometown: {props.user.location}</h5>
       <h5>{props.user.username}'s review count:</h5>
       <strong>{props.user.reviews.length}</strong>
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -2,3 +2,8 @@ export const getAvgRating = reviews => {
   let ratings = reviews.map(r => r.rating)
   return ratings.reduce((memo, rating) => memo + rating)/reviews.length
 }
+
+export const renderDate = user => {
+  let joinDate = new Date(user.created_at), options = {year: 'numeric', month: 'long' }
+  return joinDate.toLocaleString('en-US', options)
+}
