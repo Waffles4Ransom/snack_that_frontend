d8bc5106e77669655ff268742436b1075fad78ce
Jenna Ransom <jenna.ransom@gmail.com>
2020-06-30 14:59:32 -0600

    small adustments to user card and destructuring props in profile component

--- src/components/users/Profile.js
+++ src/components/users/Profile.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { renderDate } from '../../helpers/helpers'
 import { Link } from 'react-router-dom'
 
-const Profile = (props) => {
+const Profile = ({ user, currentUser }) => {
 
   // console.log(props.user ? props.user.reviews : null)
 
@@ -14,14 +14,14 @@ const Profile = (props) => {
 
   //need to grab snack data form serializer
   const renderReviews = () => {
-    return props.user.reviews.map(r => (
+    return user.reviews.map(r => (
       <li key={r.id}>{r.content}</li>
     ))
   }
 
   const addReview = () => {
-    if (props.user.id === props.currentUser.id) {
-      if (props.user.reviews.length === 0) {
+    if (user.id === currentUser.id) {
+      if (user.reviews.length === 0) {
         return <Link to='/snacks'><button>Find your first Snack to review!</button></Link>
       }
     }
@@ -29,14 +29,14 @@ const Profile = (props) => {
 
   const renderUser = () => {
     return(
-      <div>
-        <img src={props.user.profile_photo} alt={props.user.name} className='profPhoto'/>
-        <h1>{ props.user.name }</h1>
-        <h2>Snacker Alias: {props.user.username}</h2>
-        <h3>Hometown: {props.user.location}</h3>
-        <h4>Joined: {renderDate(props.user)}</h4>
-        <h4>Snack Count: {props.user.reviews.length}</h4>
-        {props.user.reviews.length > 0 ? renderReviews() : null}
+      <div className='user_profile'>
+        <img src={user.profile_photo} alt={user.name} className='profPhoto'/>
+        <h1>{user.name}</h1>
+        <h2>Snacker Alias: {user.username}</h2>
+        <h3>Hometown: {user.location}</h3>
+        <h4>Joined: {renderDate(user)}</h4>
+        <h4>Snack Count: {user.reviews.length}</h4>
+        {user.reviews.length > 0 ? renderReviews() : null}
         {/* {props.user.id === props.currentUser.id ? <Link to={`/users/${props.user.id}/edit`}><button>Edit Profile</button></Link> : null} */}
       </div>
     )
@@ -44,8 +44,8 @@ const Profile = (props) => {
 
   return( 
     <>
-    {props.user ? renderUser() : null } 
-    {props.user ? addReview() : null }
+    {user ? renderUser(user) : null } 
+    {user ? addReview() : null }
     </>
   )
 }
--- src/components/users/UserCardFront.js
+++ src/components/users/UserCardFront.js
@@ -3,9 +3,8 @@ import React from 'react'
 const UserCardFront = props => {
   return (
     <div className='snackcard' onClick={() => props.handleClick()}>
-      <h4>{props.user.name}</h4>
+      <h3>{props.user.username}</h3>
       <img src={props.user.profile_photo} alt={props.user.name} className='userCardImg'/>
-      <h5>Username: {props.user.username}</h5>
     </div>
   )
 }
