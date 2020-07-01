8dd5b24dd6c3d95efccbb167c85d08cd36ee06d1
Jenna Ransom <jenna.ransom@gmail.com>
2020-06-30 19:49:18 -0600

    reconfigure user profile

--- src/components/users/Profile.js
+++ src/components/users/Profile.js
@@ -4,22 +4,26 @@ import { Link } from 'react-router-dom'
 
 const Profile = ({ user, currentUser }) => {
 
-  // console.log(props.user ? props.user.reviews : null)
+  const listReviews = () => {
+    return (
+      <>
+        <h2>{user.id === currentUser.id ? 'My Reviews' : `${user.username}'s Reviews`}</h2>
+        {user.reviews.length > 0 ? renderReview() : 'No Reviews Yet!!'}
+      </>
+    )
+  }
 
-  // const renderSnacks = () => {
-  //   return props.user.snacks.map(s => (
-  //   <li>{s.name}</li>
-  //   ))
-  // }
-
-  //need to grab snack data form serializer
-  const renderReviews = () => {
+  const renderReview = () => {
     return user.reviews.map(r => (
-      <li key={r.id}>{r.content}</li>
+      <div key={r.id}>
+      <h4><Link to={`/snacks/${r.snack_id}`}>{r.snackname}</Link></h4>
+        <p><strong> Comment: </strong> "{r.content}"</p>
+        <p><strong> Rated: </strong> {r.rating === 0 ? ' ZERO BUTTS '  : [...Array(r.rating)].map((e, i) => <img src='/catbutt_30.png' alt='cat butt' key={i} />)}</p>
+      </div>
     ))
   }
 
-  const addReview = () => {
+  const addFirstReview = () => {
     if (user.id === currentUser.id) {
       if (user.reviews.length === 0) {
         return <Link to='/snacks'><button>Find your first Snack to review!</button></Link>
@@ -29,23 +33,29 @@ const Profile = ({ user, currentUser }) => {
 
   const renderUser = () => {
     return(
+      <>
       <div className='user_profile'>
         <img src={user.profile_photo} alt={user.name} className='profPhoto'/>
+      </div>
+      <div>
         <h1>{user.name}</h1>
         <h2>Snacker Alias: {user.username}</h2>
         <h3>Hometown: {user.location}</h3>
         <h4>Joined: {renderDate(user)}</h4>
         <h4>Snack Count: {user.reviews.length}</h4>
-        {user.reviews.length > 0 ? renderReviews() : null}
         {/* {props.user.id === props.currentUser.id ? <Link to={`/users/${props.user.id}/edit`}><button>Edit Profile</button></Link> : null} */}
       </div>
+      </>
     )
   }
 
   return( 
     <>
-    {user ? renderUser(user) : null } 
-    {user ? addReview() : null }
+      <div className='grid-container profile'>
+        {user ? renderUser(user) : "No Snacker Found!" } 
+      </div>
+      {user ? addFirstReview() : null }
+      {user ? listReviews() : null }
     </>
   )
 }
