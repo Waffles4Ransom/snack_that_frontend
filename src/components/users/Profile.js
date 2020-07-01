67ef0fb8bb3b6d32b254323a3cf1698145616aaa
Jenna Ransom <jenna.ransom@gmail.com>
2020-06-30 22:39:05 -0600

    change snack review into cards on user profile page

--- src/components/users/Profile.js
+++ src/components/users/Profile.js
@@ -7,18 +7,23 @@ const Profile = ({ user, currentUser }) => {
   const listReviews = () => {
     return (
       <>
-        <h2>{user.id === currentUser.id ? 'My Reviews' : `${user.username}'s Reviews`}</h2>
-        {user.reviews.length > 0 ? renderReview() : 'No Reviews Yet!!'}
+        <h2 className='prof_title'>{user.id === currentUser.id ? 'My Reviews' : `${user.username}'s Reviews`}</h2>
+          <div className='flex-container'>
+            {user.reviews.length > 0 ? renderReview() : 'No Reviews Yet!!'}
+          </div>
       </>
     )
   }
 
   const renderReview = () => {
     return user.reviews.map(r => (
-      <div key={r.id}>
-      <h4><Link to={`/snacks/${r.snack_id}`}>{r.snackname}</Link></h4>
-        <p><strong> Comment: </strong> "{r.content}"</p>
-        <p><strong> Rated: </strong> {r.rating === 0 ? ' ZERO BUTTS '  : [...Array(r.rating)].map((e, i) => <img src='/catbutt_30.png' alt='cat butt' key={i} />)}</p>
+      <div key={r.id} className='reviewcard'>
+      <h4>{r.snackname}</h4>
+        <p><strong>Comment:</strong></p>
+        <p>"{r.content}"</p>
+        <p><strong>Rated:</strong></p>
+        {r.rating === 0 ? ' ZERO BUTTS '  : [...Array(r.rating)].map((e, i) => <img src='/catbutt_30.png' alt='cat butt' key={i} />)}
+        <p><Link to={`/snacks/${r.snack_id}`}><button>See Snack</button></Link></p>
       </div>
     ))
   }
@@ -38,7 +43,7 @@ const Profile = ({ user, currentUser }) => {
         <img src={user.profile_photo} alt={user.name} className='profPhoto'/>
       </div>
       <div>
-        <h1>{user.name}</h1>
+        <h1 className='prof_title'>{user.name}</h1>
         <h2>Snacker Alias: {user.username}</h2>
         <h3>Hometown: {user.location}</h3>
         <h4>Joined: {renderDate(user)}</h4>
